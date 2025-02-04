7da2bcb6dbde82cc8d2db888ed8616ce0d6fe56f
unknown <betim009@gmail.com>
2025-02-04 19:07:12 -0300

    update

--- aula_3/script.js
+++ aula_3/script.js
@@ -21,6 +21,7 @@ const fetchApi = async () => {
 // Eventos
 window.addEventListener('load', async () => {
     results = await fetchApi();
+    console.log(results);
 
     results.map((element) =>
         divContainer.innerHTML += `
@@ -36,9 +37,39 @@ btnSearch.addEventListener('click', () => {
     const userCity = inputCity.value;
 
     const filterByName = results.filter((element) => element.name === userName);
+    const filterByCity = results.filter((element) => element.address.city === userCity);
+
+    console.log(filterByName.length);
+
+    if (filterByName.length === 0 && userCity === "") {
+        return divContainer.innerHTML = `
+            <p>Não foi possível encontrar um usuário com esse nome.</p>
+        `;
+    };
+
+    if (filterByCity.length === 0  && userName === "") {
+        return divContainer.innerHTML = `
+            <p>Não foi possível encontrar uma cidade com esse nome.</p>
+        `;
+    };
+
+    if (filterByName.length === 0 && filterByCity.length === 0) {
+        return divContainer.innerHTML = `
+            <p>Não foi possível encontrar uma cidade ou usuário.</p>
+        `;
+    };
+
 
     divContainer.innerHTML = "";
     filterByName.map((element) =>
+        divContainer.innerHTML += `
+            <h4>Nome: ${element.name}</h4>
+            <p>Email: ${element.email}</p>
+            <p>Cidade: ${element.address.city}</p>
+        `
+    );
+
+    filterByCity.map((element) =>
         divContainer.innerHTML += `
             <h4>Nome: ${element.name}</h4>
             <p>Email: ${element.email}</p>
